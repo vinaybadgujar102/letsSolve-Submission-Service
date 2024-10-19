809ac54b3a1f35178bd7160f9ab17746eca8108b
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-10-19 08:14:16 +0530

    add custom errors

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT,
-  REDIS_PORT: process.env.REDIS_PORT,
-  REDIS_HOST: process.env.REDIS_HOST || "6379",
+  REDIS_PORT: process.env.REDIS_PORT || "6379",
+  REDIS_HOST: process.env.REDIS_HOST || "127.0.0.1",
   NODE_ENV: process.env.NODE_ENV,
   ATLAS_DB_URL: process.env.ATLAS_DB_URL,
   PROBLEM_ADMIN_SERVICE_URL: process.env.PROBLEM_ADMIN_SERVICE_URL,
